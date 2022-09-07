ed6e978873636cc274eeeba3f0cbf13d4e8059a1
zirun-wang <zirunwang.g@gmail.com>
2022-09-07 18:00:02 +0800

    准备加入礼包性价比

---
+++ pages/packPPR/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="pack">
+        <div class="pack_unit">
+            <div class="pack_img">
+                <div class="pack_img_text">
+                </div>
+            </div>
+            <div class="pack_text">
+
+            </div>
+            <div class="pack_chart">
+                <div class="pack_chart_unit">
+                    <div class="pack_chart_unit_text"></div>
+                    <div class="pack_chart_unit_ppr"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
