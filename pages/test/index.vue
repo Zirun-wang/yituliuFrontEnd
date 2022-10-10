b6a609fc0d8f56e908a3b567d8bc8ac539323f57
sakura <1969314158@qq.com>
2022-10-10 11:13:29 +0800

    test

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,24 +1,24 @@
 <template>
     <div>
-        <el-button v-model="radio" label=true @click="radio=!radio">备选项</el-button>
-        
+       
     </div>
 </template>
 
 <script>
 
+
 export default {
   data() {
     return {   
-        radio:true
+     
     };
   },
  created() {  
+  
   },
   methods: {  
-    show(){
-        console.log(this.radio)
-    }
-  },
+    
+    },
 };
 </script>
+
