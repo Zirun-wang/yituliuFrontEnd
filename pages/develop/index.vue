61c591a7e7d41e6eaa2b355ff85a79b8859bc7bb
zirun-wang <zirunwang.g@gmail.com>
2022-11-02 18:07:54 +0800

    推上去再说

---
+++ pages/develop/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="T2_cost">
+    <table id="main">
+      <tbody>
+        <tr v-for="(singleAgent, index) in agentData" :key="index">
+          <td>
+            {{singleAgent.codeName}}
+          </td>
+          <td>
+            {{singleAgent.phases}}
+          </td>
+          <td>
+            {{singleAgent.codeName}}
+          </td>
+        </tr>
+      </tbody>
+      <!-- {{agentData}} -->
+    </table>
+
+  </div>
+</template>
+
+<script>
+  import agentData from "static/json/T2_ItemCost.json";
+
+  export default {
+    data(){
+            return {
+              agentData:agentData,
+            }
+        },
+  }
+
+</script>
+
+<style scoped>
+  .pie_all {
+    width: 450px;
+    height: 300px;
+    border-radius: 16px;
+    margin-top: -10%;
+  }
+
+</style>
