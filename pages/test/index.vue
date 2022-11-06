7678396fa880013f97cb7930801e2030542ae05d
sakura <1969314158@qq.com>
2022-11-06 13:01:02 +0800

    基建生成器反馈修复

--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
     <div class="item_card">
-      <div v-for="(item, index) in itemList" :key="index" v-show="item.cardNum <8&&item.cardNum >1">
-        <img class="item_size" :src="getImage(item.itemName)" alt="">
+      <div
+        v-for="(item, index) in itemList"
+        :key="index"
+        v-show="item.cardNum < 8 && item.cardNum > 1"
+      >
+        <img class="item_size" :src="getImage(item.itemName)" alt="" />
       </div>
     </div>
+
+    <div class="title-image">
+      <span>全干员</span><br>
+      <span>蓝材料需求统计</span><br>
+      <span>截止3.5周年活动</span><br>
+      <span>叙拉古人</span>
+    </div>
+    <div class="char-image"> <img src="/img/back/char_1028_texas2_1b.png" ></div>
   </div>
 </template>
 
@@ -12,9 +24,10 @@
 import storeApi from "@/api/store";
 
 export default {
+  layout: "poster",
   data() {
     return {
-      itemList:[]
+      itemList: [],
     };
   },
   created() {
@@ -27,9 +40,9 @@ export default {
         this.itemList = response.data;
       });
     },
-    getImage(name){
-      return "img/materials-bak/"+name+".png"
-    }
+    getImage(name) {
+      return "img/materials-bak/" + name + ".png";
+    },
   },
 };
 </script>
@@ -38,16 +51,39 @@ export default {
 .item_card {
   display: flex;
   background-color: white;
-  width: 1280;
-  height: 720;
-  overflow: hidden;
-  /* margin: 5%; */
-   flex-wrap: wrap ;  
-    background: url(~static/img/back/ep11.png);
-  background-size: 900px;
+  width: 1280px;
+  height: 720px;
+  margin: 5%;
+  flex-wrap: wrap;
+  background: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(~static/img/back/ep11.png) no-repeat 50% 50% /cover;
+  background-size: 1300px;
+  /* opacity: 0.7; */
 }
-.item_size{
+.item_size {
   margin: 5px;
-  width: 70px;
+  width: 105px;
+  opacity: 0.5;
+}
+
+.title-image {
+  /* background-color: rgb(180, 180, 180); */
+  color: rgb(255, 217, 0);
+  margin-top: -700px;
+  margin-left: 120px;
+  width: 990px;
+  font-size: 96px;
+  text-shadow: 10px 10px 10px rgb(0, 0, 0);
+  letter-spacing: 1px;
+  font-weight: 700;
+  /* font-family: STXingkai; */
+  position: relative;
+  z-index: 99;
+}
+
+.char-image{
+  position: relative;
+  z-index: 90;
+  margin-top: -500px;
+  margin-left: 400px;
 }
 </style>
