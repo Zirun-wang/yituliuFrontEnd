30c644e61a0ad3be13bbe7371bd35b3728402141
Zirun-wang <zirunwang.g@gmail.com>
2022-12-09 05:48:43 +0800

    算法公示卡模块

---
+++ pages/al_card/al_card.vue
@@ -0,0 +1,66 @@
+<template>
+    <table id="al_card">
+        <tbody>
+            <tr>
+                <td>
+                    算法代号
+                </td>
+                <td>
+                    一图流_标准 v6.0
+                </td>
+                <td>
+                    更新时间
+                </td>
+                <td>
+                    2000/00/00 12:30
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    数据源
+                </td>
+                <td>
+                    企鹅物流
+                </td>
+                <td>
+                    基准
+                </td>
+                <td>
+                    常驻关卡
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    计算引擎
+                </td>
+                <td>
+                    yituliuBackEnd
+                </td>
+                <td>
+                    样本阈值
+                </td>
+                <td>
+                    300
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    需求目标
+                </td>
+                <td>
+                    无限需求
+                </td>
+                <td>
+                    EXP系数
+                </td>
+                <td>
+                    0.625
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+  
+<style>
+
+</style>
