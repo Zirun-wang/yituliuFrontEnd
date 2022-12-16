e8842a89f9101ee4a423f2a6317181fcd5d3d743
sakura <1969314158@qq.com>
2022-12-16 12:29:47 +0800

    礼包性价比更新

--- pages/develop/create/index.vue
+++ pages/develop/create/index.vue
@@ -3,60 +3,62 @@
     <table border="0" class="pack-table">
       <tbody>
       <tr class="title—tr">
-        <td>礼包名称</td>
-        <td>礼包显示名称</td>
-        <td>礼包图片名称</td>
-        <td>礼包类型</td>
-        <td>礼包ID</td>
-        <td>礼包状态</td>
-        <td>售价</td>
-        <td>抽数</td>
-        <td>源石数</td>
-        <td>抽卡单价</td>
-        <td>源石单价</td>
-        <td>抽卡性价比百分比</td>
-        <td>总体性价比百分比</td>
-        <td>礼包备注</td>
-        <td>合成玉</td>
-        <td>源石</td>
-        <td>单抽</td>
-        <td>十连</td>
+        <td class="long-td">礼包名称</td>
+        <td class="long-td">礼包显示名称</td>
+        <td class="long-td">礼包图片名称</td>
+        <td class="long-short">礼包类型</td>
+        <td class="long-short">礼包ID</td>
+        <td class="long-short">礼包状态</td>
+        <td class="long-short">售价</td>
+        <td class="long-short">抽数</td>
+        <td class="long-short">源石数</td>
+        <td class="long-short">抽卡单价</td>
+        <td class="long-short">源石单价</td>
+        <td class="long-short">抽卡性价比百分比</td>
+        <td class="long-short">总体性价比百分比</td>
+       
+        <td class="long-short">合成玉</td>
+        <td class="long-short">源石</td>
+        <td class="long-short">单抽</td>
+        <td class="long-short">十连</td>
         <td>礼包内容</td>
       </tr>
-      <tr v-for="index in 15" :key="index" class="title—tr">
-        <td><input type="text" class="input_item" v-model="packName[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packShowName[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packImg[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packType[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packID[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packState[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packPrice[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packDraw[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packOriginium[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packRmbPerDraw[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packRmbPerOriginium[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packPPRDraw[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packPPROriginium[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="packRemark[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="gachaOrundum[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="gachaOriginium[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="gachaPermit[index-1]"/></td>
-        <td><input type="text" class="input_item" v-model="gachaPermit10[index-1]"/></td>
+      <tr v-for="index in packTestJsonLength" :key="index" class="title—tr">
+        <td><input type="text" class="input_long" v-model="packName[index-1]"/></td>
+        <td><input type="text" class="input_long" v-model="packShowName[index-1]"/></td>
+        <td><input type="text" class="input_long" v-model="packImg[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model="packType[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model.number="packID[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model.number="packState[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model.number="packPrice[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model="packDraw[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model="packOriginium[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model="packRmbPerDraw[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model="packRmbPerOriginium[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model="packPPRDraw[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model="packPPROriginium[index-1]"/></td>
+      
+        <td><input type="text" class="input_short" v-model.number="gachaOrundum[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model.number="gachaOriginium[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model.number="gachaPermit[index-1]"/></td>
+        <td><input type="text" class="input_short" v-model.number="gachaPermit10[index-1]"/></td>
         <td class="">
           <div class="div-Content">
             <div v-for="count in 15" :key="count" class="div-item">
-              <input type="text" class="input_item" v-model="packContent_item[index-1][count-1]"/>
-              <input type="text" class="input_item" v-model="packContent_count[index-1][count-1]"/>
+              <input type="text" class="input_long" v-model="packContent_item[index-1][count-1]"/>
+              <input type="text" class="input_long" v-model.number="packContent_count[index-1][count-1]"/>
             </div>
           </div>
         </td>
+        
       </tr>
+      
 
       </tbody>
     </table>
     <el-button @click="setJson">创建Json</el-button>
     <el-button @click="creatdJson">生成json</el-button>
-    <el-button ><a href="https://houduan.yituliu.site/file/export/store/pack/json?uid=111111111">下载json</a> </el-button>
+    <el-button ><a href="https://backend.yituliu.site/file/export/store/pack/json?uid=1111">下载json</a> </el-button>
   </div>
 </template>
 
@@ -69,6 +71,7 @@
     layout: "poster",
     data() {
       return {
+        packTestJsonLength:20,
         packName: [],
         packShowName: [],
         packImg: [],
@@ -157,7 +160,7 @@
     },
     methods: {
       creatdJson() {
-        buildingApi.maaBuildingJsonCreated(this.packJson, 111111111).then((response) => {
+        buildingApi.maaBuildingJsonCreated(this.packJson, 1111).then((response) => {
 
         });
       },
@@ -170,9 +173,9 @@
           map.packShowName = this.packShowName[i];
           map.packImg = this.packImg[i];
           map.packType = this.packType[i];
-          map.packID = this.packID[i];
-          map.packState = this.packState[i];
-          map.packPrice = this.packPrice[i];
+          map.packID = parseInt(this.packID[i]);
+          map.packState = parseInt(this.packState[i]);
+          map.packPrice = parseInt(this.packPrice[i]);
           map.packDraw = this.packDraw[i];
           map.packOriginium = this.packOriginium[i];
           map.packRmbPerDraw = this.packRmbPerDraw[i];
@@ -180,10 +183,10 @@
           map.packPPRDraw = this.packPPRDraw[i];
           map.packPPROriginium = this.packPPROriginium[i];
           map.packRemark = this.packRemark[i];
-          map.gachaOrundum = this.gachaOrundum[i];
-          map.gachaOriginium = this.gachaOriginium[i];
-          map.gachaPermit = this.gachaPermit[i];
-          map.gachaPermit10 = this.gachaPermit10[i];
+          map.gachaOrundum = parseInt(this.gachaOrundum[i]);
+          map.gachaOriginium = parseInt(this.gachaOriginium[i]);
+          map.gachaPermit = parseInt(this.gachaPermit[i]);
+          map.gachaPermit10 = parseInt(this.gachaPermit10[i]);
            
         
           let content = [];
@@ -211,7 +214,7 @@
 
 
       getJson() {
-
+         this.packTestJsonLength = packTestJson.length;
         for (let i = 0; i < packTestJson.length; i++) {
           // if("礼包"== packName[i]) break;
           this.packName[i] = packTestJson[i].packName;
@@ -253,30 +256,43 @@
 <style scoped>
 
 
-  /* .pack-table {
+  .pack-table {
     border-collapse: collapse;
-  } */
+  }
 
   .title—tr {
-    display: block;
-    width: auto;
-    border-bottom: solid rgb(0, 0, 0) 1px;
+
+   
+  }
+
+  .title—tr td{
+    border: solid rgb(0, 0, 0) 1px;
     margin-bottom: 10px;
+    text-align: center;
   }
 
-  .title—tr td {
-    width: 78px;
+  .short-td {
+    width: 50px;
+  }
+  .input_short {
+    width: 50px;
   }
 
-  .input_item {
-    width: 70px;
+  .long-td {
+    width: 110px;
+  }
+
+  .input_long {
+    width: 110px;
   }
 
   .div-Content {
-    height: 100px;
+    height: 80px;
     overflow: auto;
-    scrollbar-width: none; /* firefox */
-    -ms-overflow-style: none; /* IE 10+ */
+    scrollbar-width: none; 
+    /* firefox */
+    -ms-overflow-style: none; 
+    /* IE 10+ */
   }
 
   .div-item {
